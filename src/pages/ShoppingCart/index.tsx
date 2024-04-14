e3bb67178a1e1d494b893b101449036603e7ea6c
2596915059@qq.com <2596915059@qq.com>
2024-04-14 20:34:49 +0800

    feat: 功能全部完成，差动画和增加数量有个bug，

--- src/pages/ShoppingCart/index.tsx
+++ src/pages/ShoppingCart/index.tsx
@@ -1,10 +1,23 @@
 /** @format */
 
 import React, { useEffect, useState, useRef } from 'react';
-import { Checkbox, Form, Radio, Card, Button, Row, Col, InputNumber, Table, Drawer } from 'antd';
+import {
+  Checkbox,
+  Form,
+  Radio,
+  Card,
+  Button,
+  Row,
+  Col,
+  InputNumber,
+  Table,
+  Drawer,
+  Modal,
+  Spin,
+} from 'antd';
 import { DeleteOutlined } from '@ant-design/icons';
-import { useAppSelector, useAppDispatch } from '@/store/hooks';
-import { setShoppingCartData, getShoppingCartData, getTotalPrice } from '@/store/shoppingCartSlice';
+import { useAtom } from 'jotai';
+import { atomWithStorage } from 'jotai/utils';
 import { mockData } from './mockdata';
 
 import '@/styles/pages/shoppingcart.less';
@@ -13,14 +26,23 @@ const { Meta } = Card;
 
 const sizeOptions = ['XS', 'S', 'M', 'L', 'XL', 'XLL'];
 
+const allCountAtom = atomWithStorage('testCount', 0);
+const totalPriceAtom = atomWithStorage('totalPrice', 0);
+const shopCartDataAtom = atomWithStorage('shoppingCartData', []);
 const ShoppingCart = () => {
-  const shoppingCartData = useAppSelector(getShoppingCartData);
-  const totalPrice = useAppSelector(getTotalPrice);
+  const [testCount, setCount] = useAtom(allCountAtom);
+  const [shoppingCartData, setShopCartData] = useAtom(shopCartDataAtom);
+  const [totalPrice, setTotalPrice] = useAtom(totalPriceAtom);
 
-  const dispatch = useAppDispatch();
   const [form] = Form.useForm();
   const [listData, setListData] = useState<any[]>([]);
   const [visible, setVisible] = useState(false);
+  const [isModalOpenOne, setIsModalOpenOne] = useState(false);
+  const [isModalOpenTwo, setIsModalOpenTwo] = useState(false);
+  const [currentSelectSize, setSelectSize] = useState('XS');
+  const [currentSelectShop, setCurrentSelectShop] = useState({});
+  const [currentShopSize, setCurrentShopSize] = useState(['X', 'L', 'XL', 'XXL']);
+  const [spinning, setSpinning] = useState(false);
   const allListData = useRef<any[]>([]);
 
   // Mock数据
@@ -30,14 +52,30 @@ const ShoppingCart = () => {
     setListData(mockData.sort(sortFn));
     allListData.current = mockData;
   };
-
+  // 选择尺码
+  const selectSizeFun = info => {
+    setIsModalOpenTwo(true);
+    setCurrentShopSize(info.availableSizes);
+    setCurrentSelectShop(info);
+  };
+  const showLoader = () => {
+    setSpinning(true);
+    setTimeout(() => {
+      setSpinning(false);
+    }, 3000);
+  };
+  useEffect(() => {
+    showLoader();
+  }, []);
   const addToShoppingCard = info => {
     // 判断购物车中是否存在
     let isExist = false;
     let total = 0;
-    const newData = shoppingCartData.map(item => {
-      total += item.price * (item.count + 1);
-      if (item.id === info.id) {
+    let allCount = 0;
+    const newData = shoppingCartData.map((item: any) => {
+      total += item.price * item.count;
+      allCount += item.count;
+      if (item.id === info.id && item.size === currentSelectSize) {
         isExist = true;
         return { ...item, count: item.count + 1 };
       }
@@ -46,9 +84,19 @@ const ShoppingCart = () => {
     if (!isExist) {
       // TODO size的处理
       total += info.price;
-      newData.push({ id: info.id, title: info.title, count: 1, size: 'XXL', price: info.price });
+      allCount += 1;
+      newData.push({
+        id: info.id,
+        title: info.title,
+        count: 1,
+        size: currentSelectSize,
+        price: info.price,
+      });
     }
-    dispatch(setShoppingCartData({ data: newData, total }));
+    setCount(allCount);
+    setShopCartData(newData as any);
+    setTotalPrice(total);
+    // dispatch(setShoppingCartData({ data: newData, total, allCount }));
     setVisible(true);
   };
 
@@ -75,17 +123,35 @@ const ShoppingCart = () => {
       );
     }
   };
+  // 选择尺码弹窗
+  const onChange = e => {
+    setSelectSize(e.target.value);
+  };
+  const selectSizeOk = () => {
+    addToShoppingCard(currentSelectShop);
+    setIsModalOpenTwo(false);
+  };
+  const showModalOne = () => {
+    setIsModalOpenOne(true);
+  };
 
+  const handleOk = () => {
+    setIsModalOpenOne(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpenOne(false);
+    setIsModalOpenTwo(false);
+  };
   const getCardInfo = info => (
     <div className="card-info">
       <div className="card-name">{info.title}</div>
       <div className="card-price">{Number(info.price).toFixed(2)}</div>
       <div className="card-size">{(info.availableSizes || []).join(',')}</div>
-      <div>{info.description || ''}</div>
       <div style={{ marginTop: 8 }}>
         <Button
           onClick={() => {
-            addToShoppingCard(info);
+            selectSizeFun(info);
           }}
         >
           添加到购物车
@@ -131,15 +197,21 @@ const ShoppingCart = () => {
           value={value}
           onChange={val => {
             let total = 0;
-            const newShoppingCartData = shoppingCartData.map(item => {
+            let count = 0;
+            const newShoppingCartData = shoppingCartData.map((item: any) => {
               if (item.id === record.id) {
                 total += item.price * val;
+                count += item.count;
                 return { ...item, count: val };
               }
               total += item.price * item.count;
+              count += item.count;
               return { ...item };
             });
-            dispatch(setShoppingCartData({ data: newShoppingCartData, total }));
+            setCount(count + 1);
+            setShopCartData(newShoppingCartData as any);
+            setTotalPrice(total);
+            // dispatch(setShoppingCartData({ data: newShoppingCartData, total }));
           }}
         />
       ),
@@ -154,13 +226,19 @@ const ShoppingCart = () => {
           danger
           onClick={() => {
             let total = 0;
-            const newShoppingCartData = shoppingCartData.filter(item => {
+            let count = 0;
+            const newShoppingCartData = shoppingCartData.filter((item: any) => {
               if (item.id !== record.id) {
                 total += item.price * item.count;
+                count += item.count;
               }
               return item.id !== record.id;
             });
-            dispatch(setShoppingCartData({ data: newShoppingCartData, total }));
+
+            setShopCartData(newShoppingCartData as any);
+            setTotalPrice(total);
+            setCount(count);
+            // dispatch(setShoppingCartData({ data: newShoppingCartData, total }));
           }}
         >
           <DeleteOutlined /> 删除
@@ -171,28 +249,40 @@ const ShoppingCart = () => {
 
   return (
     <>
-      <Form form={form}>
-        <Form.Item name="size" label="尺寸">
-          <Checkbox.Group
-            options={sizeOptions.map(item => ({ value: item, label: item }))}
-            onChange={val => {
-              onFormChange('size', val);
-            }}
-          />
-        </Form.Item>
-        <Form.Item name="sort" label="价格">
-          <Radio.Group
-            options={[
-              { value: 'asc', label: '按价格升序' },
-              { value: 'desc', label: '按价格降序' },
-            ]}
-            onChange={e => {
-              onFormChange('sort', e.target.value);
-            }}
-            optionType="button"
-          />
-        </Form.Item>
-      </Form>
+      <div className="shopping-cart-header">
+        <div>
+          <Form form={form}>
+            <Form.Item name="size" label="尺寸">
+              <Checkbox.Group
+                options={sizeOptions.map(item => ({ value: item, label: item }))}
+                onChange={val => {
+                  onFormChange('size', val);
+                }}
+              />
+            </Form.Item>
+            <Form.Item name="sort" label="价格">
+              <Radio.Group
+                options={[
+                  { value: 'asc', label: '按价格升序' },
+                  { value: 'desc', label: '按价格降序' },
+                ]}
+                onChange={e => {
+                  onFormChange('sort', e.target.value);
+                }}
+                optionType="button"
+              />
+            </Form.Item>
+          </Form>
+        </div>
+        <div
+          className="shoping-cart"
+          onClick={() => {
+            setVisible(true);
+          }}
+        >
+          购物车{testCount}
+        </div>
+      </div>
       <div>
         <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
           {listData.map(item => (
@@ -224,11 +314,36 @@ const ShoppingCart = () => {
           columns={tableColumns}
           dataSource={shoppingCartData}
           size="small"
-          rowKey="id"
+          rowKey={record => `${record.id}-${record.size}`}
           pagination={false}
         />
-        <h1>总价格：{totalPrice}</h1>
+        <h1>总价格：{Number(totalPrice).toFixed(2)}</h1>
+        <h1>总数量：{testCount}</h1>
+        <Button onClick={showModalOne} style={{ float: 'right' }}>
+          结算
+        </Button>
       </Drawer>
+      <Modal
+        title="Basic Modal"
+        open={isModalOpenOne}
+        onOk={() => handleOk()}
+        onCancel={handleCancel}
+      >
+        <p>一共{Number(totalPrice).toFixed(2)}，确认支付吗</p>
+      </Modal>
+      <Modal
+        title="Basic Modal"
+        open={isModalOpenTwo}
+        onOk={() => selectSizeOk()}
+        onCancel={handleCancel}
+      >
+        <Radio.Group onChange={onChange} value={currentSelectSize}>
+          {currentShopSize.map(item => (
+            <Radio value={item}>{item}</Radio>
+          ))}
+        </Radio.Group>
+      </Modal>
+      <Spin spinning={spinning} fullscreen />
     </>
   );
 };
--- src/store/shoppingCartSlice.ts
+++ src/store/shoppingCartSlice.ts
@@ -7,18 +7,19 @@ export interface InitState {
   [key: string]: any;
 }
 
-const initialState: InitState = { data: [], total: 0 };
+const initialState: InitState = { data: [], total: 0 , allCount: 0 };
 
 export const shoppingCartSlice = createSlice({
   name: 'shoppingCart',
   initialState,
   reducers: {
     setShoppingCartData: (state, action: PayloadAction<any>) => {
-      const { data = [], total = 0 } = action.payload;
+      const { data = [], total = 0 ,allCount = 0} = action.payload;
       return {
         ...state,
         data,
         total,
+        allCount
       };
     },
   },
@@ -28,5 +29,6 @@ export const { setShoppingCartData } = shoppingCartSlice.actions;
 
 export const getShoppingCartData = (state: RootState) => state.shoppingCart.data;
 export const getTotalPrice = (state: RootState) => state.shoppingCart.total;
+export const getTotalCount = (state: RootState) => state.shoppingCart.allCount;
 
 export default shoppingCartSlice.reducer;
